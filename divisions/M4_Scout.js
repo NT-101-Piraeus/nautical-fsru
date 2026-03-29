7350e17686a93223e82680b1f66d099e8f4a635f
NT-101-Piraeus <contact@nauticaltechnology.gr>
2026-03-29 17:30:00 +0300

    Delete divisions/M4_Scout.js

--- divisions/M4_Scout.js
+++
@@ -1,7 +0,0 @@
-const M4_Scout = () => (
-    <div className="p-8 glass rounded-[2.5rem] text-center italic">
-        <h2 className="brand text-cyan-400 text-lg mb-4 uppercase">M4: ARRIVALS RADAR</h2>
-        <p className="text-[10px] opacity-60 animate-pulse uppercase font-black">Scanning AIS Horizon...</p>
-    </div>
-);
-window.M4_Scout = M4_Scout;
